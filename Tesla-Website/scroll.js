db168199227bff6880f5d66fd34eeeeccd8211f1
arunlalpp <arunlalpp@gmail.com>
2022-08-27 22:20:49 +0530

    fix: js error fix

--- Tesla-Website/scroll.js
+++ Tesla-Website/scroll.js
@@ -2,18 +2,22 @@ document.lastScrollPostion = 0;
 document.lastCentered = 0
 
 
-document.addEventListener('scroll',() =>{
-    const direction = window.pageYOffset - document.lastScrollPostion > 0  ? 'down' : 'up';
-    const sections = [...document.querySelectorAll('section')];
+document.addEventListener("#scroll",() =>{
+    const direction = window.pageYOffset - document.lastScrollPostion > 0  ? "down" : "up";
+    const sections = [...document.querySelectorAll(selectors, 'section')];
 
 
-    const destIndex =  direction === 'up' ? document.lastCentered - 1 : document.lastCentered
+    const destIndex =  direction === "up" ? document.lastCentered - 1 : document.lastCentered +1;
 
     if(destIndex>= 0 && destIndex < sections.length){
         console.log({destIndex,direction});
-        window.scroll(x:0, sections[destIndex].offsetTop)
+        window.scroll(sections[destIndex].offsetTop)
     }
-    sections.forEach((section,index)=>{
+
+
+    
+    sections.forEach((section,index: number ) => 
+    {
         if(window.pageYOffset === section.offsetTop){
             document.lastCentered = index;
         }

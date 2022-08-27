f471580f0968de29d9fa1d685d9b5668d84282fd
arunlalpp <arunlalpp@gmail.com>
2022-08-27 22:57:10 +0530

    fix: js error fix

--- Tesla-Website/scroll.js
+++ Tesla-Website/scroll.js
@@ -1,30 +1,32 @@
-document.lastScrollPostion = 0;
-document.lastCentered = 0
+document.lastScrollPosition = 0;
+document.lastCentered = 0;
+document.onWayTo = null;
 
+document.addEventListener('scroll', () => {
+  const direction = window.pageYOffset - document.lastScrollPosition > 0 ? 'down' : 'up';
+  const sections = [...document.querySelectorAll('section')];
 
-console.log("sreeeeeeeee")
-
-document.addEventListener("#scroll", listener,() =>{
-    const direction = window.pageYOffset - document.lastScrollPostion > 0  ? "down" : "up";
-    const sections = [...document.querySelectorAll(selectors, 'section')];
-
-
-    const destIndex =  direction === "up" ? document.lastCentered - 1 : document.lastCentered +1;
-
-    if(destIndex>= 0 && destIndex < sections.length){
-        console.log({destIndex,direction});
-        document.onWayTo = destIndex;
-
-        window.scroll(sections[destIndex].offsetTop)
+  if (document.onWayTo === null) {
+    const destIndex = direction === 'up' ? document.lastCentered - 1 : document.lastCentered + 1;
+    if (destIndex >= 0 && destIndex < sections.length) {
+      console.log({destIndex,direction});
+      document.onWayTo = destIndex;
+      window.scroll(0, sections[destIndex].offsetTop);
     }
-
-    sections.forEach((section,index ) => 
-    {
-        if(window.pageYOffset === section.offsetTop){
-            document.lastCentered = index;
-        }
-    })
+  }
 
 
-    document.lastScrollPostion = window.pageYOffset;
+  sections.forEach((section,index) => {
+    if (window.pageYOffset === section.offsetTop) {
+      document.lastCentered = index;
+      section.className = 'active';
+      if (document.onWayTo === index) {
+        document.onWayTo = null;
+      }
+    } else {
+      section.className = '';
+    }
+  })
+
+  document.lastScrollPosition = window.pageYOffset;
 })

c6c4868cd846362245c3e8c1564be43e7a6f3da8
Arun Lal PP <arunlalpp@gmail.com>
2022-11-25 22:36:48 +0530

    feat: carousel style

--- Disney+ Hotstar-website-Clone/app.js
+++ Disney+ Hotstar-website-Clone/app.js
@@ -17,4 +17,20 @@ const createSlide=()=>{
     let h1 =document.createElement('h1');
     let p =document.createElement('p');
 
+    // attaching all elements
+
+    imgElement.appendChild(document.createTextNode(''));
+    h1.appendChild(document.createTextNode(movies[slideIndex].name));
+    p.appendChild(document.createTextNode(movies[slideIndex].des));
+    content.appendChild(h1)
+    content.appendChild(p)
+    slide.appendChild(content)
+    slide.appendChild(imgElement)
+    carousel.appendChild(slide)
+
+    // setting up images which is in slider-data.js
+
+    imgElement.src=movie[slideIndex].images;
+    slideIndex++;
+
 }

9a2a1845b1a80e25a483f7f32aa5427ed0c8b56d
Arun Lal PP <arunlalpp@gmail.com>
2022-11-25 22:40:09 +0530

    feat: carousel style

--- Disney+ Hotstar-website-Clone/app.js
+++ Disney+ Hotstar-website-Clone/app.js
@@ -1,51 +1,48 @@
-let carousel = document.querySelector('.carousel');
-let sliders=[];
+let carousel = document.querySelector(".carousel");
+let sliders = [];
 
-let slideIndex=0;
+let slideIndex = 0;
 
+const createSlide = () => {
+	if (slideIndex >= DeviceMotionEvent.legth) {
+		slideIndex = 0;
+	}
 
-const createSlide=()=>{
-    if(slideIndex>=DeviceMotionEvent.legth){
-        slideIndex=0
-    }
+	// Creating dom element
 
-    // Creating dom element
+	let slide = document.createElement("div");
+	let imgElement = document.createElement("img");
+	let content = document.createElement("div");
+	let h1 = document.createElement("h1");
+	let p = document.createElement("p");
 
-    let slide =document.createElement('div');
-    let imgElement =document.createElement('img');
-    let content =document.createElement('div');
-    let h1 =document.createElement('h1');
-    let p =document.createElement('p');
+	// attaching all elements
 
-    // attaching all elements
+	imgElement.appendChild(document.createTextNode(""));
+	h1.appendChild(document.createTextNode(movies[slideIndex].name));
+	p.appendChild(document.createTextNode(movies[slideIndex].des));
+	content.appendChild(h1);
+	content.appendChild(p);
+	slide.appendChild(content);
+	slide.appendChild(imgElement);
+	carousel.appendChild(slide);
 
-    imgElement.appendChild(document.createTextNode(''));
-    h1.appendChild(document.createTextNode(movies[slideIndex].name));
-    p.appendChild(document.createTextNode(movies[slideIndex].des));
-    content.appendChild(h1)
-    content.appendChild(p)
-    slide.appendChild(content)
-    slide.appendChild(imgElement)
-    carousel.appendChild(slide)
+	// setting up images which is in slider-data.js
 
-    // setting up images which is in slider-data.js
+	imgElement.src = movie[slideIndex].images;
+	slideIndex++;
 
-    imgElement.src=movie[slideIndex].images;
-    slideIndex++;
+	// setting elements
 
-    // setting elements
+	slide.className = "slider";
+	content.className = "slider-content";
+	h1.className = "slider-title";
+	p.className = "slider-desc";
 
-    slide.className='slider';
-    content.className='slider-content';
-    h1.className='slider-title';
-    p.className='slider-desc';
+	sliders.push(slide);
 
-    sliders.push(slide);
+	// adding sliding effect
 
-    // adding sliding effect
-
-    if(sliders.length){
-        
-    }
-
-}
+	if (sliders.length) {
+	}
+};
